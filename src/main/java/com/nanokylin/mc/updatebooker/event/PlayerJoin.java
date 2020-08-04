d1f65a80c2ab56128b25e13c5f9bd19e7deefa8d
Hanbings <3219065882@qq.com>
2020-08-04 12:11:11 +0800

    :sparkles:​ Feat: Happy Lazy

--- src/main/java/com/nanokylin/mc/updatebooker/event/PlayerJoin.java
+++ src/main/java/com/nanokylin/mc/updatebooker/event/PlayerJoin.java
@@ -4,11 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.utility.MinecraftReflection;
-import com.sun.org.apache.xpath.internal.objects.XNull;
 import fr.xephi.authme.events.LoginEvent;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -25,23 +21,34 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * 好像没啥写的 一个类完成吧
+ *
  * @author Hanbings
  */
-public class PlayerJoin implements Listener{
+public class PlayerJoin implements Listener {
     /**
      * 就在这里完成它！
+     *
      * @param event authme给的登录后的触发的事件
      * @author Hanbings
      */
     @EventHandler
-    public void LoginEvent(LoginEvent event){
+    public void LoginEvent(LoginEvent event) {
         Player p = event.getPlayer();
         FileConfiguration message = YamlConfiguration.loadConfiguration(new File("plugins/UpdateBooker/message.yml"));
-        openBook(newBook("HelloWorld","UpdateBooker", ChatColor.translateAlternateColorCodes('&', message.getString("message").replace("\\n", "\n"))),p);
+        openBook(newBook("HelloWorld", "UpdateBooker", ChatColor.translateAlternateColorCodes('&', message.getString("message").replace("\\n", "\n"))), p);
 
     }
-    public ItemStack newBook(String title, String author, String... pages)
-    {
+
+    /**
+     * Lazy 太长了 仅仅是把创建书独立出来
+     *
+     * @param title  标题
+     * @param author 作者
+     * @param pages  内容
+     * @return 当然是返回书本啦
+     * @author Hanbings
+     */
+    public ItemStack newBook(String title, String author, String... pages) {
         BookMeta meta = (BookMeta) Bukkit.getItemFactory().getItemMeta(Material.WRITTEN_BOOK);
         meta.setTitle(title);
         meta.setAuthor(author);
@@ -51,18 +58,23 @@ public class PlayerJoin implements Listener{
         book.setItemMeta(meta);
         return book;
     }
-    public void openBook(ItemStack book, Player p)
-    {
+
+    /**
+     * 使用ProtocolLib发包
+     *
+     * @param book 书本啦
+     * @param p    玩家啦
+     * @author Hanbings
+     */
+    public void openBook(ItemStack book, Player p) {
         ProtocolManager pm = ProtocolLibrary.getProtocolManager();
         int slot = p.getInventory().getHeldItemSlot();
         ItemStack old = p.getInventory().getItem(slot);
         p.getInventory().setItem(slot, book);
         PacketContainer pc = pm.createPacket(PacketType.Play.Server.OPEN_BOOK);
-        try
-        {
+        try {
             pm.sendServerPacket(p, pc);
-        } catch (InvocationTargetException e)
-        {
+        } catch (InvocationTargetException e) {
             throw new RuntimeException("Cannot send open book packet " + pc, e);
         }
         p.getInventory().setItem(slot, old);
